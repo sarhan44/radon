bd536d8b439fccfcb6753c597a30eb5c349b48d6
Sarhank44 <you@example.com>
2022-06-03 09:49:05 +0530

    GetAPI Assignment 02/6/22t

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,40 +1,131 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
-
+const lodash = require('lodash');
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
-    res.send('My first ever api!')
-});
 
-router.get('/hello', function (req, res) {
+
+
+// =======================[Assignment - Get API ]===========================>>>
+
+
+// =======[ 1 ]======>>
+
+router.get('/movies', function (req, res) {
+    const movie = ['Captain America', 'Iron Man', 'Avengers', 'Spiderman']
    
-    res.send('Hello there!')
+    res.send(movie)
 });
 
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
+// =======[ 2 ]======>>
+
+// router.get('/movies/:indexNumber', function(req, res){
+//     const movie = ['Captain America', 'Iron Man', 'Avengers', 'Spiderman']
+//      res.send('Movie name is '+ movie[req.params.indexNumber])
+//     console.log("API IS WORKING")
+// })  
+// =======[ 3 ]======>>
+
+router.get('/movies/:indexNumber', function(req, res){
+    const movie = ['Captain America', 'Iron Man', 'Avengers', 'Spiderman']
+    
+    let index = movie[req.params.indexNumber]
+        if(index !== movie.length){
+            res.send(index || "Ops 404 Error - Please Enter Valid Number")
+        }
+        console.log("Response Sent")
 })
 
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
+// =======[ 4 ]======>>
+
+router.get('/films', function(req, res){
+    const films = [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+
+       
+       res.send(films)
+       console.log("Response Sent")
 })
+
+
+
+// =======[ 5 ]======>>
+
+router.get('/films/:filmId', function(req, res){
+    const films = [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+
+       let index = films[req.params.filmId - 1]
+        if(index !== films.length){
+            res.send(index|| "Ops 404 Error - Please Enter Valid ID")
+        }
+       
+       console.log("Response Sent")
+})
+
+
+
+
+// 4 :-<<====<<====<<======= Assignment/nodejs-modules =====>>===>>===>>=====>>
+
+router.get('/hello', function (req , res){
+    const months = ["January", "Februray", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    const chunky = lodash.chunk(months , 3)
+    res.send(chunky)
+    console.log("Response Sent")
+})
+
+router.get('/odd', function (req , res){
+    const oddNum = [1,3,5,7,9,11,13,15,17,19]
+    const taily = lodash.tail(oddNum, 9)
+    console.log ("Response Sent")
+    res.send(taily)
+})
+
+router.get('/number', function (req , res){
+    const e = [1,2,3]
+    const a =  [3,4,5]
+    const b = [4,5,6] 
+    const c = [6,7,8] 
+    const d = [7,8,9]
+    const merge = lodash.union(a,b,c,d,e)
+    console.log("Response Sent")
+    res.send(merge)
+})
+
+
+router.get('/objectPairs', function (req , res){
+    const a =  [["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+
+    const pairs = lodash.fromPairs(a)
+    console.log("Response Sent")
+
+    res.send(pairs)
+})
+
+
+
 
 
 module.exports = router;
