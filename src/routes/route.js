3a579b7acc2dfc73113ef15026c10cd2d8e3cfd9
Sarhank44 <you@example.com>
2022-06-06 11:12:58 +0530

    mongodbSession1

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+const UserModel= require("../models/bookModel.js")
+const bookController= require("../controllers/bookController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+router.post('/createBook', bookController.createBook)
 
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
+router.get('/showBookData',bookController.showBookData)
 
 module.exports = router;
