9db1fe02855b6124c443feeb04a3937a6cc282e1
Sarhank44 <you@example.com>
2022-06-16 17:00:22 +0530

    myself

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,18 +1,22 @@
-  const jwt = require("jsonwebtoken");
-  const userModel = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel");
 
+// =====================[ Create Users]===============
 
-    // =====================[ Create Users]===============
-
-  const createUser = async function (abcd, xyz) {
-    let data = abcd.body;
+const createUser = async function (req, res) {
+  try {
+    let data = req.body;
     let savedData = await userModel.create(data);
-    xyz.send({msg: savedData,});
-  };
+    res.status(404).send({ msg: savedData });
+  } catch (err) {
+    res.status(400).send({ msg: "error", error: err.message });
+  }
+};
 
-  // =====================[ login Users]===============
+// =====================[ login Users]===============
 
-  const loginUser = async function (req, res) {
+const loginUser = async function (req, res) {
+  try {
     let userName = req.body.emailId;
     let password = req.body.password;
     let user = await userModel.findOne({
@@ -20,83 +24,115 @@
       password: password,
     });
     if (!user)
-      return res.send({status: false, msg: "username or the password is not corerct",});
-      // ---------[Create Token JWT]---------
+      return res.status(404).send({
+        status: false,
+        msg: "username or the password is not corerct",
+      });
+    // ---------[Create Token JWT]---------
     let token = jwt.sign(
       {
         userId: user._id.toString(),
-        batch: "radon",               //payload data
+        batch: "radon", //payload data
         organisation: "FunctionUp",
-    },
+      },
       "functionup-radon" //Secret Key
-);
+    );
     res.setHeader("x-auth-token", token);
-    res.send({status: true,token: token,});
-  };
+    res.status(201).send({ status: true, token: token });
+  } catch (err) {
+    res.send({ msg: "error", err });
+  }
+};
+// =====================[ Get Users]===============
 
-
-  // =====================[ Get Users]===============
-
-  const getUserData = async function (req, res) {
+const getUserData = async function (req, res) {
+  try {
     let userId = req.params.userId;
     let user = await userModel.findById(userId);
-    let userDetails = await userModel.findById(userId)
+    let userDetails = await userModel.findById(userId);
     if (!user) {
-      return res.send("No such user exists");
+      return res.status(404).send("No such user exists");
     }
-    res.send({status: true, data: userDetails,});
-  };
+    res.status(200).send({ status: true, data: userDetails });
+  } catch (err) {
+    res.send({ msg: "error", err });
+  }
+};
 
-  // =====================[ Update Users]===============
+// =====================[ Update Users]===============
 
-  const updateUser = async function (req, res) {
+const updateUser = async function (req, res) {
+  try {
     let userData = req.body;
     let userId = req.params.userId;
     let user = await userModel.findById(userId);
     if (!user) {
-      return res.send("No such user exists");
+      return res.status(404).send("No such user exists");
     }
-    let updatedUser = await userModel.findOneAndUpdate({_id: userId,},userData);
-    res.send({status: true,data: updatedUser,});
-  };
+    let updatedUser = await userModel.findOneAndUpdate(
+      { _id: userId },
+      userData
+    );
+    res.status(200).send({ status: true, data: updatedUser });
+  } catch (err) {
+    res.send({ msg: "error", err });
+  }
+};
 
-  // =================[ Delete User ]==============
+// =================[ Delete User ]==============
 
-  const deleteUser = async function (req, res) {
+const deleteUser = async function (req, res) {
+  try {
     let userId = req.params.userId;
     let user = await userModel.findById(userId);
     if (!user) {
-      return res.send("No such user exists");
+      return res.status(404).send("No such user exists");
     }
 
     let userData = req.body;
-    let updatedUser = await userModel.findOneAndUpdate({_id: userId,},userData);
-    res.send({status: true,data: updatedUser,});
-  };
+    let updatedUser = await userModel.findOneAndUpdate(
+      { _id: userId },
+      userData
+    );
+    res.status(201).send({ status: true, data: updatedUser });
+  } catch (err) {
+    res.send({ msg: "error", err });
+  }
+};
 
-  // =================[ Update Post ]==============
+// =================[ Update Post ]==============
 
-  const postMessage = async function (req, res) {
-    let message = req.body.message
-    
-    let user = await userModel.findById(req.params.userId)
-    if(!user) return res.send({status: false, msg: 'No such user exists'})
-    
-    let updatedPosts = user.posts
+const postMessage = async function (req, res) {
+  try {
+    let message = req.body.message;
+
+    let user = await userModel.findById(req.params.userId);
+    if (!user)
+      return res
+        .status(404)
+        .send({ status: false, msg: "No such user exists" });
+
+    let updatedPosts = user.posts;
     //add the message to user's posts
-    updatedPosts.push(message)
-    let updatedUser = await userModel.findOneAndUpdate({_id: user._id},{posts: updatedPosts}, {new: true})
+    updatedPosts.push(message);
+    let updatedUser = await userModel.findOneAndUpdate(
+      { _id: user._id },
+      { posts: updatedPosts },
+      { new: true }
+    );
 
     //return the updated user document
-    return res.send({status: true, data: updatedUser})
-}
+    return res.status(201).send({ status: true, data: updatedUser });
+  } catch (err) {
+    res.send({ msg: "error", err });
+  }
+};
 
+// =====================[ Exports ]===============
 
-  // =====================[ Exports ]===============
-
-  module.exports.createUser = createUser;
-  module.exports.getUserData = getUserData;
-  module.exports.updateUser = updateUser;
-  module.exports.loginUser = loginUser;
-  module.exports.deleteUser = deleteUser;
-  module.exports.postMessage = postMessage
+module.exports.createUser = createUser;
+module.exports.getUserData = getUserData;
+module.exports.updateUser = updateUser;
+module.exports.loginUser = loginUser;
+module.exports.deleteUser = deleteUser;
+module.exports.postMessage = postMessage;
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,28 +1,53 @@
 const jwt = require("jsonwebtoken");
 
+// =====================[authentication]================
 
-const authenticate = async (req ,res, next) =>{
-
+const authenticate = async (req, res, next) => {
+  try {
     let token = req.headers["x-Auth-token"];
     if (!token) token = req.headers["x-auth-token"];
-    if (!token) 
-    return res.status(401).send({ status: false, msg: "token must be present" });
-    next()
- 
-}
+    if (!token)
+      return res.status(401).send({
+        status: false,
+        msg: "token must be present",
+      });
+  } catch (err) {
+    res.status(500).send({
+      msg: "Error",
+      error: err,
+    });
+  }
+  next();
+};
 
+// =====================[authorization]================
 
 const authorise = (req, res, next) => {
-    let token = req.headers["x-auth-token"]
+  try {
+    let token = req.headers["x-auth-token"];
     let decodedToken = jwt.verify(token, "functionup-radon");
-    if (!decodedToken) return res.status(403).send({ status: false, msg: "token is invalid" });
-    let findUserId = decodedToken.userId 
-    let checkUser = req.params.userId
+    if (!decodedToken)
+      return res.status(403).send({
+        status: false,
+        msg: "token is invalid",
+      });
+    let findUserId = decodedToken.userId;
+    let checkUser = req.params.userId;
     if (checkUser !== findUserId)
-     return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+      return res.status(403).send({
+        status: false,
+        msg: "User logged is not allowed to modify the requested users data",
+      });
+  } catch (err) {
+    res.status(500).send({
+      msg: "Error",
+      error: err,
+    });
+  }
+  next();
+};
 
-    next()
-}
+// =====================[Exports]================
 
-module.exports.authenticate = authenticate
-module.exports.authorise = authorise
+module.exports.authenticate = authenticate;
+module.exports.authorise = authorise;
